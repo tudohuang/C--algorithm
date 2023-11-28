986932017abceee8b4020218e0836a7e6d89a145
TudoHuang <88125758+tudohuang@users.noreply.github.com>
2023-11-28 20:28:17 +0800

    Create BubbleSort.cpp

---
+++ BubbleSort.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+void bubbleSort(std::vector<int>& arr) {
+    int n = arr.size();
+    for (int i = 0; i < n - 1; i++)    
+        for (int j = 0; j < n - i - 1; j++)
+            if (arr[j] > arr[j + 1])
+                std::swap(arr[j], arr[j + 1]);
+}
+
+int main() {
+    std::vector<int> arr = {64, 34, 25, 12, 22, 11, 90};
+    bubbleSort(arr);
+    for (int i = 0; i < arr.size(); i++)
+        std::cout << arr[i] << " ";
+    return 0;
+}
