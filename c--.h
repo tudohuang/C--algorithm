d9a77715ed4247a36547cb7bb2bde79369fb7780
TudoHuang <88125758+tudohuang@users.noreply.github.com>
2023-12-01 10:44:11 +0800

    Create c--.h

---
+++ c--.h
@@ -0,0 +1,12 @@
+#define ct cout 
+#define cn cin
+#define import(std) using namespace std;
+#define it int
+#define fl float
+#define dl doule
+#define ch char
+#define ll long
+#define rep(i,n) for(int i;i<n;i++)
+#define break; br
+#define elif else if
+#define main int main()
